e0c8839a717cf3e7027af1c0874cbc14877719e4
LaughingLeader <laughingleader@gmail.com>
2021-05-05 12:07:51 -0500

    Formatting tweak

--- Mods/SuperEnemyUpgradeOverhaul_e21fcd37-daec-490d-baec-f6f3e83f1ac9/Story/RawFiles/Lua/Shared/Data/Corruption/TagBoostEntries.lua
+++ Mods/SuperEnemyUpgradeOverhaul_e21fcd37-daec-490d-baec-f6f3e83f1ac9/Story/RawFiles/Lua/Shared/Data/Corruption/TagBoostEntries.lua
@@ -1,11 +1,15 @@
+local tb = Classes.TagBoost
+
+local DefaultParams = {DisplayInTooltip = true}
+
 local entries = {
-	LLENEMY_ShadowBonus_Madness = Classes.TagBoost:Create("LLENEMY_ShadowBonus_Madness","LLENEMY_ShadowBonus_Madness_Enabled", {HasToggleScript = true, DisplayInTooltip = true}),
-	LLENEMY_ShadowBonus_StunDefense = Classes.TagBoost:Create("LLENEMY_ShadowBonus_StunDefense","LLENEMY_ShadowBonus_StunDefense_Enabled", {DisplayInTooltip = true}),
-	LLENEMY_ShadowBonus_DotCleanser = Classes.TagBoost:Create("LLENEMY_ShadowBonus_DotCleanser","LLENEMY_ShadowBonus_DotCleanser_Enabled", {DisplayInTooltip = true}),
-	LLENEMY_ShadowBonus_CursedFire = Classes.TagBoost:Create("LLENEMY_ShadowBonus_CursedFire","LLENEMY_ShadowBonus_CursedFire_Enabled", {DisplayInTooltip = true}),
-	LLENEMY_ShadowBonus_ShockingRain = Classes.TagBoost:Create("LLENEMY_ShadowBonus_ShockingRain","LLENEMY_ShadowBonus_ShockingRain_Enabled", {DisplayInTooltip = true}),
-	LLENEMY_ShadowBonus_SlipperyRogue = Classes.TagBoost:Create("LLENEMY_ShadowBonus_SlipperyRogue","LLENEMY_ShadowBonus_SlipperyRogue_Enabled", {DisplayInTooltip = true}),
-	LLENEMY_ShadowBonus_DefensiveStart = Classes.TagBoost:Create("LLENEMY_ShadowBonus_DefensiveStart","LLENEMY_ShadowBonus_DefensiveStart_Enabled", {DisplayInTooltip = true}),
+	LLENEMY_ShadowBonus_Madness = tb:Create("LLENEMY_ShadowBonus_Madness","LLENEMY_ShadowBonus_Madness_Enabled", {HasToggleScript = true, DisplayInTooltip = true}),
+	LLENEMY_ShadowBonus_StunDefense = tb:Create("LLENEMY_ShadowBonus_StunDefense","LLENEMY_ShadowBonus_StunDefense_Enabled", DefaultParams),
+	LLENEMY_ShadowBonus_DotCleanser = tb:Create("LLENEMY_ShadowBonus_DotCleanser","LLENEMY_ShadowBonus_DotCleanser_Enabled", DefaultParams),
+	LLENEMY_ShadowBonus_CursedFire = tb:Create("LLENEMY_ShadowBonus_CursedFire","LLENEMY_ShadowBonus_CursedFire_Enabled", DefaultParams),
+	LLENEMY_ShadowBonus_ShockingRain = tb:Create("LLENEMY_ShadowBonus_ShockingRain","LLENEMY_ShadowBonus_ShockingRain_Enabled", DefaultParams),
+	LLENEMY_ShadowBonus_SlipperyRogue = tb:Create("LLENEMY_ShadowBonus_SlipperyRogue","LLENEMY_ShadowBonus_SlipperyRogue_Enabled", DefaultParams),
+	LLENEMY_ShadowBonus_DefensiveStart = tb:Create("LLENEMY_ShadowBonus_DefensiveStart","LLENEMY_ShadowBonus_DefensiveStart_Enabled", DefaultParams),
 }
 
 if Ext.IsClient() then
